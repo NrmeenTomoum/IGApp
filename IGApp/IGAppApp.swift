b672cda8f7b1c0e9712ade12379e267901a613c7
Nermeen Tomoum <nrmeen.tomoum@alumni.fci-cu.edu.eg>
2025-03-19 19:18:43 +0200

    Initial Commit

---
+++ IGApp/IGAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  IGAppApp.swift
+//  IGApp
+//
+//  Created by Nermeen Tomoum on 19/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct IGAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ IGAppTests/IGAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  IGAppTests.swift
+//  IGAppTests
+//
+//  Created by Nermeen Tomoum on 19/03/2025.
+//
+
+import Testing
+@testable import IGApp
+
+struct IGAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
